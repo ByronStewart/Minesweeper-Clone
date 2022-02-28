79f251fbd6b89e301334ad9af69e3b71554343d0
ByronStewart <41535561+ByronStewart@users.noreply.github.com>
2022-02-28 13:53:38 +1000

    refactor(frontend): pagination component
    refactored the onboarding page to have a pagination component
    fixed the title page styling

--- frontend/src/views/BaseLayout/IndexPage/IndexPage.tsx
+++ frontend/src/views/BaseLayout/IndexPage/IndexPage.tsx
@@ -2,20 +2,27 @@ import { Link } from "react-router-dom"
 
 const IndexPage: React.FC = () => {
   return (
-    <div className="h-screen bg-red-200">
-      <div className="flex flex-col justify-between">
+    <div className="flex flex-col justify-between h-full">
+      <div className="mx-6 mt-10">
         <h1 className="text-5xl">Modern Minesweeper</h1>
         <p className="mt-28 font-thin">
           Play the classic game in a whole new way...
         </p>
-        {/* Need Image here or animation */}
+      </div>
+      {/* Need Image here or animation */}
+      <div className="flex justify-evenly mb-10">
         <Link
-          className="mt-10 px-4 py-2 bg-slate-400 text-xl rounded-sm active:bg-slate-600 hover:bg-slate-500"
+          className="px-4 py-2 bg-slate-400 text-xl rounded-sm active:bg-slate-600 hover:bg-slate-500"
+          to="/instructions"
+        >
+          Tell me how
+        </Link>
+        <Link
+          className="px-4 py-2 bg-slate-400 text-xl rounded-sm active:bg-slate-600 hover:bg-slate-500"
           to="/game"
         >
           Lets play
         </Link>
-        <Link to="/instructions">Tell me how</Link>
       </div>
     </div>
   )
--- frontend/src/views/BaseLayout/InstructionsPage/InstructionsPage.tsx
+++ frontend/src/views/BaseLayout/InstructionsPage/InstructionsPage.tsx
@@ -3,6 +3,7 @@ import { IconContext } from "react-icons"
 import { GoChevronLeft, GoChevronRight } from "react-icons/go"
 import { useSwipeable } from "react-swipeable"
 import { debounce } from "lodash"
+import { Pagination } from "./Components/Pagination"
 
 type OnboardingState = {
   page: number
@@ -17,7 +18,6 @@ const reducer = (
   state: OnboardingState,
   action: OnboardingAction
 ): OnboardingState => {
-  console.log(state, action)
   switch (action.dir) {
     case "back":
       if (state.page == 0) {
@@ -37,13 +37,13 @@ const reducer = (
 }
 
 export const InstructionsPage: React.FC = () => {
-  const [state, dispatch] = useReducer(reducer, { page: 1 })
+  const [tabState, dispatchTabAction] = useReducer(reducer, { page: 0 })
   const swipeHandlers = useSwipeable({
-    onSwipedLeft: (e) => {
-      dispatch({ dir: "forward" })
+    onSwipedLeft: () => {
+      dispatchTabAction({ dir: "forward" })
     },
     onSwipedRight: () => {
-      dispatch({ dir: "back" })
+      dispatchTabAction({ dir: "back" })
     },
     preventDefaultTouchmoveEvent: true,
   })
@@ -53,76 +53,39 @@ export const InstructionsPage: React.FC = () => {
         Modern <br></br>Minesweeper
       </h2>
       {/* Instruction pane */}
-      <div className={`mx-10 text-lg ${state.page !== 0 ? "hidden" : ""}`}>
-        <p>Swipe left to flag</p>
-        <p>Swipe right to clear</p>
-        <p>Swipe down to reveal</p>
-        <p>Swipe up to cancel</p>
-      </div>
-      <div className={`mx-10 text-lg ${state.page !== 1 ? "hidden" : ""}`}>
-        <p>
-          When you reveal a tile the tile will say how many mines are adjacent
-          to it.
-        </p>
-      </div>
-      <div className={`mx-10 text-lg ${state.page !== 2 ? "hidden" : ""}`}>
-        <p>You will if you reveal all the tiles without revealing a mine</p>
-        <br />
-        <p>If you reveal a mine...</p>
-        <p>Boom... game over...</p>
-      </div>
-      <div className="flex justify-between items-center">
-        {/* Menu Bar */}
-        <IconContext.Provider
-          value={{
-            size: "2.5em",
-          }}
-        >
-          {state.page > 0 ? (
-            <div onClick={() => dispatch({ dir: "back" })} className="p-5">
-              <GoChevronLeft color={"#000"} />
-            </div>
-          ) : (
-            <div className="p-5">
-              <GoChevronLeft color="#999" />
-            </div>
-          )}
-
-          <div className="flex justify-start gap-3 items-center p-5 pl-8">
-            <div
-              className={`${
-                state.page == 0
-                  ? "bg-blue-500 border-blue-900"
-                  : "bg-gray-500 border-gray-900"
-              } w-5 h-5 rounded-full border `}
-            ></div>
-            <div
-              className={`${
-                state.page == 1
-                  ? "bg-blue-500 border-blue-900"
-                  : "bg-gray-500 border-gray-900"
-              } w-5 h-5 rounded-full border `}
-            ></div>
-            <div
-              className={`${
-                state.page == 2
-                  ? "bg-blue-500 border-blue-900"
-                  : "bg-gray-500 border-gray-900"
-              } w-5 h-5 rounded-full border `}
-            ></div>
+      <div className="flex-grow flex justify-center flex-col">
+        <div>
+          <div
+            className={`mx-10 text-lg ${tabState.page !== 0 ? "hidden" : ""}`}
+          >
+            <p>Swipe left to flag</p>
+            <p>Swipe right to clear</p>
+            <p>Swipe down to reveal</p>
+            <p>Swipe up to cancel</p>
           </div>
-
-          {state.page < 2 ? (
-            <div onClick={() => dispatch({ dir: "forward" })} className="p-5">
-              <GoChevronRight />
-            </div>
-          ) : (
-            <div className="p-5">
-              <GoChevronRight color="#999" />
-            </div>
-          )}
-        </IconContext.Provider>
+          <div
+            className={`mx-10 text-lg ${tabState.page !== 1 ? "hidden" : ""}`}
+          >
+            <p>
+              When you reveal a tile the tile will say how many mines are
+              adjacent to it.
+            </p>
+          </div>
+          <div
+            className={`mx-10 text-lg ${tabState.page !== 2 ? "hidden" : ""}`}
+          >
+            <p>You will if you reveal all the tiles without revealing a mine</p>
+            <br />
+            <p>If you reveal a mine...</p>
+            <p>Boom... game over...</p>
+          </div>
+        </div>
       </div>
+      <Pagination
+        goBack={() => dispatchTabAction({ dir: "back" })}
+        goForward={() => dispatchTabAction({ dir: "forward" })}
+        page={tabState.page}
+      />
     </div>
   )
 }
