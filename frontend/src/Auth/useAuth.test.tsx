1779fba59276229e29c781837aa604911ab14dbe
Byron Stewart <byronstewart22@gmail.com>
2022-04-25 17:37:48 +1000

    fix: added more file descriptions to the readme

--- frontend/src/Auth/useAuth.test.tsx
+++ frontend/src/Auth/useAuth.test.tsx
@@ -1,50 +1,50 @@
-import { renderHook, act } from "@testing-library/react-hooks"
-import { ProvideAuth, useAuth } from "./useAuth"
+import { renderHook, act } from "@testing-library/react-hooks";
+import { ProvideAuth, useAuth } from "./useAuth";
 
 describe.skip("useAuth tests", () => {
-  let wrapper: React.FC
+  let wrapper: React.FC;
   beforeAll(() => {
-    wrapper = ({ children }) => <ProvideAuth>{children}</ProvideAuth>
-  })
+    wrapper = ({ children }) => <ProvideAuth>{children}</ProvideAuth>;
+  });
 
   it("should be logged out by default", () => {
-    const { result } = renderHook(() => useAuth(), { wrapper })
-    expect(result.current.user).toBe(false)
-  })
+    const { result } = renderHook(() => useAuth(), { wrapper });
+    expect(result.current.user).toBe(false);
+  });
 
   // need mock api
   it("should login a user", async () => {
     const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
       wrapper,
-    })
+    });
 
-    result.current.signIn("bob9@test.com", "bob9", () => {})
+    result.current.signIn("bob9@test.com", "bob9", () => {});
 
     await waitForNextUpdate({
       timeout: 5000,
-    })
+    });
     expect(result.current.user).toMatchObject({
       username: "bob9",
-    })
-  })
+    });
+  });
 
   // need mock api
-  it.skip("should register a user", () => {
-    const { result } = renderHook(() => useAuth(), { wrapper })
+  it("should register a user", () => {
+    const { result } = renderHook(() => useAuth(), { wrapper });
     act(() => {
-      result.current.register("billy", "billy@gmail.com", "abcd", () => {})
-    })
+      result.current.register("billy", "billy@gmail.com", "abcd", () => {});
+    });
     expect(result.current.user).toStrictEqual({
       username: "Bob",
       id: 2,
-    })
-  })
+    });
+  });
 
   it("should logout a user", () => {
-    const { result } = renderHook(() => useAuth(), { wrapper })
+    const { result } = renderHook(() => useAuth(), { wrapper });
     act(() => {
-      result.current.signOut()
-    })
-    expect(result.current.user).toBe(false)
-  })
-})
+      result.current.signOut();
+    });
+    expect(result.current.user).toBe(false);
+  });
+});
