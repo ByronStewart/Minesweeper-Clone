be1df29be3cfd198554c4d28d112383202789916
Byron Stewart <byronstewart22@gmail.com>
2022-07-03 19:01:59 +1000

    minor secret fix

--- capstone/settings.py
+++ capstone/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -51,7 +51,7 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    os.environ['FRONTEND_ORIGIN']
+    os.environ.get('FRONTEND_ORIGIN')
 ]
 
 CORS_URLS_REGEX = r"^/api/.*$"
