c1d4eb36b69ef3a442ac3d1b012857de74402791
Byron Stewart <byronstewart22@gmail.com>
2022-07-03 17:43:20 +1000

    fixed security vulnerability

--- capstone/settings.py
+++ capstone/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-528vyn&4tsp*jn&^ej%s%ur&ueh=gn!vf6q9i!8!&pnej)rzu&'
+with open('/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
