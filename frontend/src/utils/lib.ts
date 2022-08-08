c33f0329cf12888f719e8a250cc7efa640c221f9
Byron Stewart <byronstewart22@gmail.com>
2022-08-08 16:38:32 +1000

    fix(frontend): bug fixes and tests to utils

--- frontend/src/utils/lib.ts
+++ frontend/src/utils/lib.ts
@@ -1,19 +1,9 @@
-export function secondsToMinsAndSeconds(seconds: number) {
-  let mins = 0
-  while (true) {
-    let tempSeconds = Math.floor(seconds / 60)
-    if (tempSeconds <= 0) {
-      break
-    }
-    seconds = tempSeconds
-    mins += 1
-  }
-  return {
-    mins,
-    seconds,
-  }
+export function secondsToMinsAndSeconds(input: number) {
+  const mins = Math.floor(input / 60);
+  const seconds = input % 60;
+  return { mins, seconds };
 }
 
 export function formatMinsAndSeconds(mins: number, seconds: number) {
-  return `${mins}:${seconds >= 10 ? seconds : `0${seconds}`}`
+  return `${mins}:${seconds >= 10 ? seconds : `0${seconds}`}`;
 }
