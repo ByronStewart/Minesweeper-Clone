8cb74d5e7589626edefc2ee5077fcccb00f92a4a
Byron Stewart <byronstewart22@gmail.com>
2022-09-11 09:37:52 +1000

    feat: with poetry

--- frontend/src/common/utils.ts
+++
@@ -1,9 +0,0 @@
-export function secondsToMinsAndSeconds(input: number) {
-  const mins = Math.floor(input / 60);
-  const seconds = input % 60;
-  return { mins, seconds };
-}
-
-export function formatMinsAndSeconds(mins: number, seconds: number) {
-  return `${mins}:${seconds >= 10 ? seconds : `0${seconds}`}`;
-}
--- frontend/src/components/layouts/GameLayout.tsx
+++
@@ -1,6 +0,0 @@
-import { Outlet } from "react-router-dom"
-
-interface Props {}
-export const MinesweeperLayout: React.FC<Props> = ({ children }) => {
-  return <Outlet />
-}
