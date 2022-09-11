5bcc05df5361671910e2edef54be88078ffc737e
Byron Stewart <byronstewart22@gmail.com>
2022-09-11 13:16:31 +1000

    feat: with healthcheck

--- capstone/urls.py
+++ capstone/urls.py
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import include, path
+from rest_framework import status
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse(status=status.HTTP_200_OK)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('healthcheck', health_check)
 ]
